40d32c35065800f3c1a7381a83a5e8f4d3ba89c0
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-08 07:33:53 +0300

    squash!

--- src/utils/Lock.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-import AwaitLock from "await-lock";
-
-export const explorerLock: AwaitLock = new AwaitLock();
